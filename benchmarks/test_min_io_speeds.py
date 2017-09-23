bdb488536064f34a4ec24c8856e13aa8026b8bf6
pete-dnae <pete.howard@dnae.com>
2017-09-23 23:05:38 +0100

    Added tests for copy, read and iterate over big file.

--- benchmarks/test_min_io_speeds.py
+++ benchmarks/test_min_io_speeds.py
@@ -2,19 +2,22 @@
 This module foo.
 """
 
-import unittest
 
 # Standard lib imports
+import unittest
 import tempfile
 from pathlib import Path
 import timeit
+import sys
+import shutil
+from functools import partial
 
 # Addon imports
 import humanfriendly as hf
 
 FNAME = 'file_to_read'
 
-QUITE_BIG = int(1e9)
+QUITE_BIG = int(3e9)
 
 
 class Foo(unittest.TestCase):
@@ -23,11 +26,7 @@ class Foo(unittest.TestCase):
     def setUp(self):
         # A working directory that gets automatically cleaned up.
         self.dir = tempfile.TemporaryDirectory()
-
-        # Make a file we can read back in later.
-        self.file_to_read = Path(self.dir.name).joinpath(FNAME)
-        with self.file_to_read.open('w+b') as f:
-            f.write(b'\x00' * QUITE_BIG)
+        self.dname = self.dir.name
 
 
     def tearDown(self):
@@ -39,25 +38,45 @@ class Foo(unittest.TestCase):
     """
     def test_something(self):
         in_mem_data_to_write = b'\x00' * QUITE_BIG
-        file = Path(self.dir.name).joinpath(FNAME)
+        file = Path(self.dname).joinpath(FNAME)
 
-        def writing_operation():
-            nonlocal file
-            with file.open('w+b') as f:
+        def write_op(f):
+            with f.open('w+b') as f:
                 f.write(in_mem_data_to_write)
 
-        def reading_operation():
-            nonlocal file
-            with file.open() as f:
-                bytes_read = f.read()
+        def read_op(f):
+            bytes_read = self._read_file_into_mem(f)
 
-        time_to_write = timeit.timeit(writing_operation, number = 1)
-        time_to_read = timeit.timeit(reading_operation, number = 1)
-        print('Seconds to open and write a %s file: %s' % 
+        def copy_op(f):
+            dest = Path(self.dname).joinpath('copied_file').as_posix()
+            shutil.copyfile(f.as_posix(), dest)
+
+        def iter_op(bytes):
+            for byte in bytes:
+                pointless = byte | 0xFF
+
+
+        time_to_write = timeit.timeit(partial(write_op, file), number = 1)
+        time_to_read = timeit.timeit(partial(read_op, file), number = 1)
+        time_to_copy = timeit.timeit(partial(copy_op, file), number = 1)
+
+        bytes = self._read_file_into_mem(file)
+        time_to_iter = timeit.timeit(partial(iter_op, bytes), number = 1)
+
+        print('Seconds to open and write a %s file:  %s' % 
             (hf.format_size(QUITE_BIG), time_to_write))
-        print('Seconds to open and read a %s file: %s' % 
+        print('Seconds to open and read a %s file:   %s' % 
             (hf.format_size(QUITE_BIG), time_to_read))
+        print('Seconds to copy a %s file:            %s' % 
+            (hf.format_size(QUITE_BIG), time_to_copy))
+        print('Seconds to iterate through a %s file: %s' % 
+            (hf.format_size(QUITE_BIG), time_to_iter))
 
+
+    def _read_file_into_mem(self, fname):
+        with fname.open('rb') as f:
+            bytes_read = f.read()
+        return bytes_read
 
 
 if __name__ == '__main__':
